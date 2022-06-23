ae0b787ad8fbf35c1d09b30c7522f7d84dcd3a83
boningggg <wang.boning@outlook.com>
2022-06-23 20:18:39 +1000

    [Script for Neovim] including nerd font installation

--- configure.sh
+++ configure.sh
@@ -27,16 +27,6 @@ common_config ()
 	fi
 }
 
-dependences_config ()
-{
-	echo "- Install basic dependences for Neovim."
-	echo "    - ripgrep"
-	echo "    - fd"
-	echo "    - clang"
-	echo "    - clang-format"
-	echo "    - openjdk"
-}
-
 macos_config ()
 {
 	echo "- Detecting you are using darwin os, use brew to install dependences for Neovim."
@@ -46,6 +36,9 @@ macos_config ()
 	sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 	yes | brew upgrade
 	yes | brew autoremove
+
+	# Install FiraCode nerd font
+	yes | brew tap homebrew/cask-fonts && brew install --cask font-fira-code-nerd-font
 }
 
 linux_config ()
@@ -55,27 +48,44 @@ linux_config ()
 	sudo apt install -y $DEPENDENCES
 	sudo apt upgrade -y
 	sudo apt autoremove -y
+
+	# Install FiraCode nerd font
+	wget -P ${HOME}/Downloads -nc https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+	unzip -o ${HOME}/Downloads/FiraCode.zip -d ${HOME}/.local/share/fonts
+	fc-cache -fv
+	rm ${HOME}/Downloads/FiraCode.zip
 }
 
-
-
-main ()
-{	
-	echo "========== Neovim Configuration =========="
-	common_config
-	dependences_config
+dependences_config ()
+{
+	echo "- Install basic dependences for Neovim."
+	echo "    - ripgrep"
+	echo "    - fd"
+	echo "    - clang"
+	echo "    - clang-format"
+	echo "    - openjdk"
 	# Because different operating systems use different package managements, 
 	# so we need to use different configurations depending on type of OS.
 	case "$MACHINE" in
 		darwin) macos_config ;;
 		linux)  linux_config ;;
 	esac
+}
+
+main ()
+{	
+	echo "========== Neovim Configuration =========="
+	common_config
+	dependences_config
 	echo ""
 	echo "Having installed basic dependences for Neovim."
-	echo "Now you need to start Neovim and install nvim-packages, run:"
+	echo "1. You need to change the font and theme of terminal to FiraCode Nerd Font Mono and light."
+	echo "2. You need to start Neovim and install nvim-packages and lsp, run:"
 	echo "    \$ nvim"
 	echo "And type:"
 	echo "    :PackerSync"
+	echo "    :LspInstall clangd jdtls rust_analyzer"
+	echo "3. Restart the terminal!"
 	echo "========== Good Jobs! =========="
 }
 
